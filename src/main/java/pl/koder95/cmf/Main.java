b1cceb3c1b7f177861ffa97b51c8aa1383234441
Kamil Jan Mularski <kammul@outlook.com>
2024-06-21 15:19:33 +0200

    Create Main.java

---
+++ src/main/java/pl/koder95/cmf/Main.java
@@ -0,0 +1,12 @@
+package pl.koder95.cmf;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Catholic Moveable Feasts by Kamil Jan Mularski [Koder95]. See more: https://koder95.pl/");
+        System.out.println("—   It is starting with arguments: " + Arrays.deepToString(args));
+        System.out.println();
+    }
+}
