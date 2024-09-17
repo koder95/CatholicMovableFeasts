72a6481f633276e4195030219be699202873ddd7
Kamil Jan Mularski <kammul@outlook.com>
2024-09-17 21:11:26 +0200

    Dokumentacja dla klasy JSON

--- src/main/java/pl/koder95/cmf/format/JSON.java
+++ src/main/java/pl/koder95/cmf/format/JSON.java
@@ -6,18 +6,38 @@ import pl.koder95.cmf.FinalMFR;
 import java.time.LocalDate;
 import java.util.*;
 
+/**
+ * Klasa zawiera narzędzia dotyczące formatu JSON.
+ *
+ * @author Kamil Mularski
+ * @see Loader
+ * @see #loader(String)
+ * @see #toJson(List)
+ * @see #toJson(List, boolean)
+ */
 public class JSON {
 
     private JSON() {}
 
+    /**
+     * Zwraca obiekt wczytujący dane zapisane w formacie JSON.
+     *
+     * @param source dane zapisane w formacie JSON
+     * @return obiekt wczytujący
+     */
     public static Loader loader(String source) {
         return new Loader(JsonParser.parseString(source));
     }
 
+    /**
+     * Klasa przechowuje wczytująca obiekty w formacie JSON i umożliwiająca zwrócenie ich w formie listy obiektów typu {@link FinalMFR}.
+     * Stworzenie instancji klasy powinno się odbywać przez wywołanie metody {@link JSON#loader(String)}, ponieważ
+     * konstruktor jest zabezpieczony.
+     */
     public static class Loader {
         private final JsonElement main;
 
-        public Loader(JsonElement main) {
+        private Loader(JsonElement main) {
             this.main = main;
         }
 
@@ -54,10 +74,21 @@ public class JSON {
         }
     }
 
+    /**
+     * Zapisuje listę {@link FinalMFR} do formatu JSON.
+     * @param mfrs lista obiektów do zapisania
+     * @return dane zapisane w formacie JSON
+     */
     public static String toJson(List<FinalMFR> mfrs) {
         return toJson(mfrs, false);
     }
 
+    /**
+     * Zapisuje listę {@link FinalMFR} do formatu JSON określając sposób zapisu.
+     * @param mfrs lista obiektów do zapisania
+     * @param prettyPrinting jeśli {@code true} wtedy zapis jest przyjazny dla człowieka, w przeciwnym razie – zapis pozbawiony zbędnych znaków
+     * @return dane zapisane w formacie JSON
+     */
     public static String toJson(List<FinalMFR> mfrs, boolean prettyPrinting) {
         GsonBuilder builder = new GsonBuilder();
         if (prettyPrinting) builder = builder.setPrettyPrinting();
