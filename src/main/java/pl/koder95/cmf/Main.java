9e48f0485ef3e56ef644567056c9cbc7c909d8f4
Kamil Jan Mularski <kammul@outlook.com>
2024-09-17 23:21:11 +0200

    Dodanie podstawowej opcji CL pomocy (-h)

--- src/main/java/pl/koder95/cmf/Main.java
+++ src/main/java/pl/koder95/cmf/Main.java
@@ -1,5 +1,7 @@
 package pl.koder95.cmf;
 
+import org.apache.commons.cli.*;
+
 import java.util.Arrays;
 
 public class Main {
@@ -8,5 +10,18 @@ public class Main {
         System.out.println("Catholic Moveable Feasts by Kamil Jan Mularski [Koder95]. See more: https://koder95.pl/");
         System.out.println("—   It is starting with arguments: " + Arrays.deepToString(args));
         System.out.println();
+
+        Options options = new Options();
+        options.addOption("h", "help", false, "wyświetla pomoc programu");
+        CommandLineParser parser = new DefaultParser(false);
+        try {
+            CommandLine cl = parser.parse(options, args);
+            if (cl.hasOption("h")) {
+                HelpFormatter hf = new HelpFormatter();
+                hf.printHelp("cmf", options);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
