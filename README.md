cd35351fc5744cd7f3c747f08a67e1e42a0a66fb
Kamil Jan Mularski <kammul@outlook.com>
2024-10-03 23:30:17 +0200

    Zapisywanie w formacie JSON za pomocą CL

--- README.md
+++ README.md
@@ -10,3 +10,29 @@ A simple and useful generator of important christian dates.
     <version>0.1.1</version>
 </dependency>
 ```
+### Command line execution
+Prints a command line help:
+```shell
+java -jar cmf.jar -h
+```
+#### Run without arguments (default run)
+```shell
+java -jar cmf.jar
+```
+It causes generating a movable feasts row for the current year and saves as JSON file in the current location.
+It formats a file name according to the pattern:<a name="default-filename"/>
+```regexp
+(\d{4})-(\d{2})-(\d{2})_(\d{2})(\d{2})(\d{2})\.cmf\.json
+```
+e.g. `2024-10-04_150923.cmf.json`.
+#### Run with options
+Available options:
+
+| Option       | Short | Default<br/>value                      | Description                                                                                                                                                  |
+|--------------|-------|----------------------------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| `--dir`      | `-d`  | the current directory                  | It determines a directory of an output file.                                                                                                                 |
+| `--filename` | `-n`  | [default file name](#default-filename) | It determines a file name of an output file.                                                                                                                 |
+| `--format`   | `-f`  | `json`                                 | It determines format for the output file. It is one of:<ul><li>`json`/`prettyjson`</li></ul>                                                                 |
+| `--help`     | `-h`  | not applicable                         | It prints help.                                                                                                                                              |
+| `--years`    | `-y`  | the current year                       | It determines a list of year for a MFR generator. Numbers can be separated by dot (e.g. `2024.2028.2050`) or determine a range of values (e.g. `2024-2050`). |
+
--- src/main/java/pl/koder95/cmf/Main.java
+++ src/main/java/pl/koder95/cmf/Main.java
@@ -1,10 +1,25 @@
 package pl.koder95.cmf;
 
 import org.apache.commons.cli.*;
+import pl.koder95.cmf.format.JSON;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
+
+    private Main() {}
 
     public static void main(String[] args) {
         System.out.println("Catholic Moveable Feasts by Kamil Jan Mularski [Koder95]. See more: https://koder95.pl/");
@@ -13,15 +28,72 @@ public class Main {
 
         Options options = new Options();
         options.addOption("h", "help", false, "wyświetla pomoc programu");
+        options.addOption("d", "dir", true, "określa katalog dla zapisania pliku");
+        options.addOption("n", "filename", true, "określa nazwę pliku");
+        options.addOption(Option.builder("f")
+                .longOpt("format")
+                .hasArg()
+                .desc("określa format danych wyjściowych")
+                .build());
+        options.addOption(Option.builder("y")
+                .longOpt("years")
+                .hasArg()
+                .type(int[].class)
+                .converter(string -> {
+                    System.out.println("Converting: " + string);
+                    String digits = "(\\d)+";
+                    if (string.matches(digits)) {
+                        return new int[] { Integer.parseInt(string) };
+                    }
+                    int dash = string.indexOf('-');
+                    if (dash > 0) {
+                        int begin = Integer.parseInt(string.substring(0, dash));
+                        int end = Integer.parseInt(string.substring(dash + 1));
+                        return IntStream.rangeClosed(begin, end).toArray();
+                    } else {
+                        String[] years = string.split("\\.");
+                        System.out.println("Parts: " + Arrays.deepToString(years));
+                        return Arrays.stream(years).filter(s -> s.matches(digits)).mapToInt(Integer::parseInt).toArray();
+                    }
+                })
+                .build());
         CommandLineParser parser = new DefaultParser(false);
         try {
             CommandLine cl = parser.parse(options, args);
             if (cl.hasOption("h")) {
                 HelpFormatter hf = new HelpFormatter();
                 hf.printHelp("cmf", options);
+            } else {
+                String dir = cl.hasOption("dir")? cl.getOptionValue("dir") : "./";
+                String filename = cl.hasOption("filename")? cl.getOptionValue("filename") : defaultFilename();
+                String format = cl.hasOption("format")? cl.getOptionValue("format") : "json";
+                int[] years = cl.hasOption("years")? cl.getParsedOptionValue("years") : new int[] { LocalDate.now().getYear() };
+                List<FinalMFR> result = generate(years);
+                save(result, dir, filename, format);
             }
-        } catch (ParseException e) {
+        } catch (ParseException | IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static String defaultFilename() {
+        LocalDateTime now = LocalDateTime.now();
+        String formatted = now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String adapted = formatted.replace('T', '_').replace(":", "");
+        String clipped = adapted.substring(0, adapted.indexOf('.'));
+        return clipped + ".cmf";
+    }
+
+    private static List<FinalMFR> generate(int... years) {
+        System.out.println("Generating for years: " + Arrays.toString(years));
+        return IntStream.of(years).sorted().mapToObj(MFR_Basic::new).map(MFR_Basic::toStable).map(MFR_Stable::toFinal).collect(Collectors.toList());
+    }
+
+    private static void save(List<FinalMFR> list, String dir, String filename, String format) throws IOException {
+        if (format.endsWith("json")) {
+            String json = JSON.toJson(list, format.replace("json", "").equals("pretty"));
+            Path destin = Paths.get(dir, filename + ".json");
+            Files.write(destin, Collections.singleton(json), StandardCharsets.UTF_8);
         }
     }
 }
